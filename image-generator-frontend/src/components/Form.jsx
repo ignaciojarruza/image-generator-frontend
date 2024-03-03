6d08000c523b2d5e75b84df2de83a0a2cf00399d
ignaciojarruza <85847657+ignaciojarruza@users.noreply.github.com>
2024-03-03 15:40:55 -0500

    [Feature] API Calls to Node.js backend
    - link to generated image returned (image_url)
    - async->await

--- image-generator-frontend/src/components/Form.jsx
+++ image-generator-frontend/src/components/Form.jsx
@@ -3,21 +3,37 @@ import styles from "./form.module.css";
 
 export default function Form() {
   const [image_description, setDescription] = useState("");
+  const [image_url, setImageUrl] = useState("");
+
+  const generateImage = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/generate/${image_description}`
+      );
+      const data = await response.json();
+      console.log(data.image_url);
+    } catch (error) {
+      console.error("Error generating Image:", error);
+    }
+  };
   function handleSubmit(e) {
     e.preventDefault();
+    generateImage();
   }
 
   return (
-    <form className={styles.modernForm} onSubmit={handleSubmit}>
-      <input
-        className={styles.modernInput}
-        type="text"
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="Enter Image Description..."
-      />
-      <button className={styles.modernButton} type="submit">
-        Generate!
-      </button>
-    </form>
+    <div>
+      <form className={styles.modernForm} onSubmit={handleSubmit}>
+        <input
+          className={styles.modernInput}
+          type="text"
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="Enter Image Description..."
+        />
+        <button className={styles.modernButton} type="submit">
+          Generate!
+        </button>
+      </form>
+    </div>
   );
 }
